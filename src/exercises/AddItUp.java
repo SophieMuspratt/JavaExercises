1a4a6b707a48a9aac2511b84cbf1b447b2207017
Sophie Muspratt <smuspratt@hotmail.co.uk>
2016-10-14 09:21:55 +0100

    Refactored Code w/ Comments

--- src/exercises/AddItUp.java
+++ src/exercises/AddItUp.java
@@ -1,51 +1,51 @@
 package exercises;
 
 import java.math.BigDecimal;
-//Remove unused imports
-import java.text.DecimalFormat;
+//Add Static ZERO import
+import static java.math.BigDecimal.ZERO;
 import java.util.List;
 
 public class AddItUp {
 
-	//listTest is not a good name
-	public static BigDecimal add(List<Object> listTest) {
-		//No initialising number
-		BigDecimal number = new BigDecimal(0);
-		//Use BigDecimal.ZERO - static import!
-		BigDecimal total = new BigDecimal(0);
+	// listTest is not a good name
+	public BigDecimal add(List<Object> numbers) {
+		// Use BigDecimal.ZERO - static import!
+		BigDecimal total = ZERO;
 
-		//If not needed, if used, be sure to handle positive first
-		if (!listTest.isEmpty()) {
-
-			//Use enhanced for loop
-			for (int i = 0; i < listTest.size(); i++) {
-				//Using try/catch too early!
-				try {
-					//Can extract if statements to a private method
-					if (listTest.get(i) instanceof BigDecimal) {
-						number = (BigDecimal) listTest.get(i);
-						// ROUND_HALF_EVEN can be moved outside if and for loops
-						number = number.setScale(2, BigDecimal.ROUND_HALF_EVEN);
-					} else if (listTest.get(i) instanceof String) {
-						//Can extract string handling to a private method
-						number = new BigDecimal((String) listTest.get(i));
-					} else if (listTest.get(i) instanceof Double) {
-						number = new BigDecimal((Double) listTest.get(i));
-						// Again ROUND_HALF_EVEN can be moved
-						number = number.setScale(2, BigDecimal.ROUND_HALF_EVEN);
-					} else if (listTest.get(i) instanceof Integer) {
-						number = new BigDecimal((Integer) listTest.get(i));
-					} else {
-						number = new BigDecimal(0);
-					}
-				} catch (NumberFormatException e) {
-					number = new BigDecimal(0);
-				}
-
-				total = total.add(number);
-
-			}
+		// If not needed, if used, be sure to handle positive first
+		// Use enhanced for loop
+		for (Object number : numbers) {
+			number = convertAndReturnBigDecimal(number);
+			total = total.add((BigDecimal) number);
 		}
-		return total;
+		// ROUND_HALF_EVEN can be moved outside if and for loops
+		return total.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+	}
+
+	private Object convertAndReturnBigDecimal(Object number) {
+		// No initialising number
+		BigDecimal convertedNumber = ZERO;
+		// Can extract if statements to a private method
+		if (number instanceof BigDecimal) {
+			convertedNumber = (BigDecimal) number;
+		} else if (number instanceof String) {
+			// Can extract string handling to a private method
+			// Using try/catch too early!
+			convertedNumber = (BigDecimal) stringHandling(number);
+		} else if (number instanceof Double) {
+			convertedNumber = new BigDecimal((Double) number);
+		} else if (number instanceof Integer) {
+			convertedNumber = new BigDecimal((Integer) number);
+		}
+		return convertedNumber;
+	}
+
+	private Object stringHandling(Object number) {
+		try {
+			number = new BigDecimal((String) number);
+		} catch (NumberFormatException e) {
+			number = ZERO;
+		}
+		return number;
 	}
 }
