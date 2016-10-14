a7df06cb29b43130e0b58a3e50607232007c9c25
Sophie Muspratt <smuspratt@hotmail.co.uk>
2016-10-14 09:24:29 +0100

    Removing Comments and Tidying Code

--- src/exercises/AddItUp.java
+++ src/exercises/AddItUp.java
@@ -7,36 +7,26 @@ import java.util.List;
 
 public class AddItUp {
 
-	// listTest is not a good name
 	public BigDecimal add(List<Object> numbers) {
-		// Use BigDecimal.ZERO - static import!
 		BigDecimal total = ZERO;
 
-		// If not needed, if used, be sure to handle positive first
-		// Use enhanced for loop
 		for (Object number : numbers) {
 			number = convertAndReturnBigDecimal(number);
 			total = total.add((BigDecimal) number);
 		}
-		// ROUND_HALF_EVEN can be moved outside if and for loops
 		return total.setScale(2, BigDecimal.ROUND_HALF_EVEN);
 	}
 
 	private Object convertAndReturnBigDecimal(Object number) {
-		// No initialising number
 		BigDecimal convertedNumber = ZERO;
-		// Can extract if statements to a private method
-		if (number instanceof BigDecimal) {
+		if (number instanceof BigDecimal)
 			convertedNumber = (BigDecimal) number;
-		} else if (number instanceof String) {
-			// Can extract string handling to a private method
-			// Using try/catch too early!
+		else if (number instanceof String)
 			convertedNumber = (BigDecimal) stringHandling(number);
-		} else if (number instanceof Double) {
+		else if (number instanceof Double)
 			convertedNumber = new BigDecimal((Double) number);
-		} else if (number instanceof Integer) {
+		else if (number instanceof Integer)
 			convertedNumber = new BigDecimal((Integer) number);
-		}
 		return convertedNumber;
 	}
 
